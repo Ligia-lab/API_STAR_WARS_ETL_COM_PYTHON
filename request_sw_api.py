3e30a302ac88696af862432a8653b22d7869d602
Ligia <ligiacamara92@gmail.com>
2025-06-03 16:26:03 -0300

    add função para fazer request da url

---
+++ request_sw_api.py
@@ -0,0 +1,9 @@
+from sw_api import SWExtraction
+
+url = 'https://swapi.dev/api/planets/'
+
+extract = SWExtraction(api_url=url)
+extract.fetch_data()
+
+for planeta in extract.results:
+    print(planeta['name'], '-', planeta['climate'])
--- sw_api.py
+++ sw_api.py
@@ -8,3 +8,13 @@ class SWExtraction:
     def __init__(self, api_url):
         self.api_url = api_url
         
+        self.results = []
+        
+        
+    def fetch_data(self):
+        response = requests.get(self.api_url, verify=False)
+        if response.status_code == 200:
+            self.results = response.json().get('results', [])
+            print('Dados recebidos')
+        else:
+            raise Exception('Falha ao acessar dados')
